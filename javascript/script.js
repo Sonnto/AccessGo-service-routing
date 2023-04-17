cc44b8cd34718b88475ae449fc3ce822a9497746
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-17 00:40:16 -0400

    Add function to return to previous "view"

--- javascript/script.js
+++ javascript/script.js
@@ -29,3 +29,38 @@ function generateRoute() {
     "generateRoute function is activated and service elements should be gone and routing elements should be showing"
   );
 }
+
+function goBack() {
+  console.log("back button clicked");
+  const serviceTitle = document.getElementById("service-title");
+  const serviceContainer = document.getElementById("service-container");
+  const routingTitle = document.getElementById("routing-title");
+  const routingContainer = document.getElementById("routing-container");
+  const profileHead = document.getElementById("profile-head");
+
+  // show service elements
+  serviceTitle.style.display = "block";
+  serviceContainer.style.display = "flex";
+  profileHead.style.display = "flex";
+
+  // hide routing elements
+  routingTitle.style.display = "none";
+  routingContainer.style.display = "none";
+
+  console.log(
+    "goBack function is activated and routing elements should be gone and service elements should be showing"
+  );
+}
+
+const backButton = document.getElementById("back-button");
+
+backButton.addEventListener("click", function () {
+  const routingTitle = document.getElementById("routing-title");
+  const routingContainer = document.getElementById("routing-container");
+  if (
+    routingTitle.style.display === "block" &&
+    routingContainer.style.display === "block"
+  ) {
+    goBack();
+  }
+});
